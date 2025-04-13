3604f71ce69cfd5c1fedef58df000dee4efd14e1
Denis MadD <ldamadd@yandex.ru>
2025-04-13 17:18:54 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/HEro";
 import AboutSection from "./Components/AboutSection/AboutSection";
@@ -8,10 +10,17 @@ import EmployeesSection from "./Components/EmployeesSection/EmployeesSection";
 import { ContactSection } from "./Components/ContactSection/ContactSection";
 import Footer from "./Components/Footer/Footer";
 import VideoPlayer from "./Components/VideoPlayer/VideoPlayer";
+import arrow from "./assets/arrow.png";
 
 const App = () => {
   const [playerState, setPlayerState] = useState(false);
+  const [hideArrowUp, setHideArrowUp] = useState(true);
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY >= 900 ? setHideArrowUp(false) : setHideArrowUp(true);
+    });
+  });
   return (
     <div>
       <Navbar />
@@ -27,6 +36,11 @@ const App = () => {
         <ContactSection />
         <Footer />
       </div>
+      <Link to="hero" smooth={true} offset={0} duration={500}>
+        <div className={`arrow-up btn${hideArrowUp ? " hide" : ""}`}>
+          <img src={arrow} alt="arrow up" />
+        </div>
+      </Link>
       <VideoPlayer playerState={playerState} setPlayerState={setPlayerState} />
     </div>
   );
